b1cd367beb0dd18a1e41913f9a6c0447424fcdf7
chandrakant-dc <chandrakant@developmentcart.com>
2026-04-02 18:49:21 +0530

    CHORE : cors

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,10 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/admin", adminRoutes);
