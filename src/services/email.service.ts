11d93b97d180c88862fd85620b4d5c60412abded
chandrakant-dc <chandrakant@developmentcart.com>
2026-04-06 13:56:11 +0530

    CHORE : update email service smtp

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,17 +2,17 @@ import nodemailer from "nodemailer";
 
 export const sendOtpEmail = async (email: string, otp: string) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
+        host: "smtp-relay.brevo.com",
         port: 587,
         secure: false,
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: process.env.BREVO_USER,
+            pass: process.env.BREVO_PASS,
         },
     });
 
     await transporter.sendMail({
-        from: `"Vikas Circle" <${process.env.EMAIL_USER}>`,
+        from: `"Vikas Circle" <${process.env.BREVO_USER}>`,
         to: email,
         subject: "Your OTP Code",
         html: `<h3>Your OTP is: ${otp}</h3>`,
